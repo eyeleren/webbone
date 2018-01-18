88ea8553bcf763f9b9d3117024183a2813823421
Unknown <lorenzo.cercato.96@gmail.com>
2018-01-18 16:59:05 +0100

    Link primo livello e css vario

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,7 @@
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("outer");
+  x = document.getElementsByClassName("none");
   if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
